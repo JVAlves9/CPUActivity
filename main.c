23fbf1bbc5b96cd275154a3587b1054ff3f54635
JVAlves9 <d-jvictor@hotmail.com>
2020-07-14 21:43:00 -0300

    tentativa de descobrir algo

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cpuid.h>
-
+#include <sys/sysinfo.h>
+#include <unistd.h>
 
 int main (){
+    const double megabyte = 1024*1024;
+
+    struct sysinfo si;
+
+    printf("%d\n",sysinfo(&si));
+    printf("%f\n",si.totalhigh);
+    system("free");
+    double totalram = si.totalram/megabyte;
+    double free_ram = (si.bufferram/megabyte+si.freeram/megabyte+si.sharedram/megabyte);
+    double perc_free_ram = (free_ram/totalram)*100;
+
+    printf("free: %5.1f\ntotal: %5.1f\n%f%%",free_ram,totalram,perc_free_ram);
     printf("\n");
     int a,b,c,d;
     __cpuid(0,a,b,c,d);
