3ff4d16d09a6a788603c12a839ca60749254900f
JVAlves9 <d-jvictor@hotmail.com>
2020-07-30 21:02:53 -0300

    something went wrong in calculate, but the rest is working

--- uptime.c
+++ uptime.c
@@ -3,6 +3,25 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/sysinfo.h>
+#include <regex.h>
+#include <dirent.h>
+#include <time.h>
+typedef struct 
+{
+    char ** directories;    //array of strins containing the processess' PIDs 
+    size_t size;    //array size
+    size_t bytes;   //number of bytes used
+}dirs;
+
+typedef struct
+{
+    char  * pid;
+    char * comm;
+    long ticks1;
+    long ticks2;
+    long startime;
+}process;
+
 
 void readUptime(float *used){
     FILE *uptime = fopen("/proc/uptime","r");   //get the file containing the active and idle times of the cpu
@@ -117,31 +136,101 @@ long * procStat(long idle[], int cpus){
     return r;
 }
 
-char * directoriesInProc(){
-    char * r;
+dirs PIDsInProc(){
+    dirs dirs;  //struct with an array of PIDs
+    int i = 0;
+    DIR * dir = opendir("/proc");   //opening the process folder 
+    struct dirent *dir_access; 
+    regex_t regex;
+
+    if(dir == NULL){
+        printf("Couldn't open directory");
+        exit(-1);
+    }
+
+    if( regcomp(&regex,"[0-9]+",REG_EXTENDED) != 0){    //regular expression will make sure that tthe name of the folder is a number, thus it is a PID
+        printf("regcomp problem");
+        exit(-1);
+    }
+    
+    dirs.size = 0;
+    dirs.bytes = 0;
+    dirs.directories = malloc(1);
+
+    while( (dir_access = readdir(dir)) != NULL ){//go though the files and folders in this directory   // DT_DIR confirms that it is in fact a folder
+        if( dir_access->d_type == DT_DIR && regexec(&regex,dir_access->d_name,0,NULL,REG_EXTENDED)==0 ){//regexec checks if it matches with the regex var
+            dirs.bytes+= (strlen(dir_access->d_name)+1)*4;      //bytes needed - pointers and chars
+            dirs.directories = realloc(dirs.directories,dirs.bytes);    //increase the allocatedd memory for the upcoming string
+            dirs.directories[dirs.size] = dir_access->d_name;
+            dirs.size++;
+        }
+    }
+
+    closedir(dir);
+    return dirs;
+}
+
+char * command(char pid[]){     //get the name of the process command
+    int i = 0;
+    char * cmd;
+    char pro[20] = "/proc/";
+    char c, temp[20];
+    strcat(pro,pid);
+    strcat(pro,"/comm");
+    FILE * comm = fopen(pro,"r");
+
+    if( comm == NULL){
+        exit(EXIT_FAILURE);
+    }
+
+    c = getc(comm);
+
+    for (int i = 0; c!=EOF; i++)
+    {
+        temp[i] = c;
+        c = getc(comm);
+    }
+    cmd = malloc(strlen(temp)+1);
+    strcpy(cmd,temp);
+    
+    return cmd;
 }
 
 void calculate(){
     float up1 =0, calcp = 0.0;
     int cpus = get_nprocs(), i;
     long  idles1[cpus+1], idles2[cpus+1], *used1, *used2;
-    unsigned long process_ticks1, process_start_time = 0, process_ticks2;
     float calc=0,calc0=0, total, used/*,calc1=0,calc2=0, calc3=0*/;
     char pid[6]="";
     long hz = sysconf(_SC_CLK_TCK);
+
+    dirs dirs = PIDsInProc();
+    process pro[dirs.size];
     
-    scanf("%s",&pid);
+    for( i = 0; i < dirs.size; i++ ){
+        pro[i].pid = dirs.directories[i];
+        pro[i].ticks1 = 0;
+        pro[i].ticks2 = 0;
+        pro[i].startime = 0;
+        pro[i].comm = command(pro[i].pid);
+    }
 
     readUptime(&up1);   
-    stat(&process_ticks1,&process_start_time,pid);
+    for(i=0; i < dirs.size; i++){
+        stat(&pro[i].ticks1,&pro[i].startime,pro[i].pid);   //get all processes ticks
+    }
     used1 = procStat(idles1,cpus);//first values
     sleep(1);
-    stat(&process_ticks2,&process_start_time,pid);
+    for(i=0; i < dirs.size; i++){
+        stat(&pro[i].ticks2,&pro[i].startime,pro[i].pid);
+    }
     used2 = procStat(idles2,cpus);//second values
+    
+    for(i =0 ; i < dirs.size; i++){
+        calcp =(float) ( pro[i].ticks2 - pro[i].ticks1 ) / hz ; //usage of the processes | time passed is 1s, so no need to divide
 
-    calcp =(float) ( process_ticks2 - process_ticks1 ) / hz ; //usage of the process | time passed is 1s, so no need to divide
-
-    printf("per used : %.2f %%\n",100 * calcp);
+        printf("per used : %.2f %%\n",100 * calcp);
+    }
 
     for(i = 0; i < cpus+1; i++ ){   //per usage of each cpu, the first is from all cpus
         total = (idles2[i] - idles1[i] + used2[i] - used1[i]);
